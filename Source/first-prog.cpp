99c6b7d42386468aaa019c06de14fa7446790615
Matthew Younger <matthew.younger@icloud.com>
2018-10-12 13:23:41 -0500

    NC

--- Source/first-prog.cpp
+++ Source/first-prog.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-using namespace std; 
+using namespace std;
 int main()
 {
 	cout << "My first C++ program." << endl;
  	cout << "The Sum of 3 and 3 = " << 6 << endl;
- 	cout << "7 + 8 " << 7 + 8 << endl; 
+ 	cout << "7 + 8 " << 7 + 8 << endl;
        return 0;
-}       
+}
