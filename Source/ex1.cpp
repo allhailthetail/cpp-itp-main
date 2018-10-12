705eb97ffaf540d43806564bc946c369f3442780
Matthew Younger <matthew.younger@icloud.com>
2018-10-12 09:25:49 -0500

    Adding some dang new examples today... :)

---
+++ Source/ex1.cpp
@@ -0,0 +1,14 @@
+// Sample C++ program
+#include <iostream>
+using namespace std;
+int main()
+{
+/* You have multiple options for doing new lines.
+Either insert an escape character, as shown, or declare an end of line
+*/
+cout << "Hello, there! \n How about a new line??" << endl;
+cout << "Now let's try some tabs!!\ttab\ttab..." << endl;
+cout << "\n\tTab to new line...";
+cout << "\n\tAgain..." << endl;
+return 0;
+}
---
+++ Source/name-age-gpa.cpp
@@ -0,0 +1,11 @@
+// program displays name, age, and GPA
+#include <iostream>
+using namespace std;
+
+int main()
+{
+cout << "My Name is: Matthew Younger";
+cout << "\n\tAge: " << 35;
+cout << "\n\tGPA: " << "ur mom..." << endl;
+return 0;
+}
