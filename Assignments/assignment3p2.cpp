efafcaae78f9332192fbc89a3794ec2e37c30055
Matthew Younger <matthew.younger@icloud.com>
2018-11-02 22:58:39 -0500

    Verified this is working according to the lab...  Success!!

--- Assignments/assignment3p2.cpp
+++ Assignments/assignment3p2.cpp
@@ -1,15 +1,26 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 
 int main(){
   int minutes;
-  double amountDue = 0;   //amount due initializes at zero
+  double
+    amountDue = 0,   //amount due initializes at zero
+    amountStage1,
+    amountStage2,
+    amountBase;
   const double            //constants for business rates:
     B_BASE = 99.99,
     B_STAGE1 = 0.45,
-    B_STAGE1_OFFSET = 170.01,
+    B_STAGE1_OFFSET = 170.01, B_STAGE1_LIMIT = 144.99,
     B_STAGE2 = 0.99,
     B_STAGE2_OFFSET = 548.01;
+  const double            //constants for regular consumer rates
+    R_BASE = 29.99,
+    R_STAGE1 = .55,
+    R_STAGE1_OFFSET = 80.01, R_STAGE1_LIMIT = 167.49,
+    R_STAGE2 = .88,
+    R_STAGE2_OFFSET = 228.5;;
 
   char custType;
   string phoneNumber;
@@ -35,20 +46,53 @@ int main(){
   cout << "Enter the phone usage in minutes: ";
   cin >> minutes;
 
-  //perform calculations
+  //perform calculations using peacewise-defined functions
   switch (custType) {
-    //business rate - using peacewise defined functions
     case 'b':
-      //used no more than 600 minutes:
-      amountDue = (minutes <= 600 && minutes) ? B_BASE : amountDue;
-      //used no more than 700 minutes:
-      amountDue = (minutes > 600 && minutes <= 700) ? (B_STAGE1 * minutes - B_STAGE1_OFFSET) : amountDue;
-      //used more than 700 minutes:
-      amountDue = (minutes > 700) ? B_STAGE2 * minutes - B_STAGE2_OFFSET : amountDue;
+      //determine total amount due:
+      amountBase = B_BASE;
+      if (minutes <= 600) {
+        amountDue = B_BASE;
+        amountStage1 = 0;
+        amountStage2 = 0;
+      } else if (minutes <= 700) {
+        amountDue = B_STAGE1 * minutes - B_STAGE1_OFFSET;
+        amountStage1 = amountDue - B_BASE;
+        amountStage2 = 0;
+      } else {
+        amountDue = B_STAGE2 * minutes - B_STAGE2_OFFSET;
+        amountStage1 = B_STAGE1_LIMIT - B_BASE;
+        amountStage2 = amountDue - B_BASE - amountStage1;
+      }
       break;
     case 'r':
-      //
-
+      //determine total amount due:
+      amountBase = R_BASE;
+      if (minutes <= 200) {
+        amountDue = R_BASE;
+        amountStage1 = 0;
+        amountStage2 = 0;
+      } else if (minutes <= 450) {
+        amountDue = R_STAGE1 * minutes - R_STAGE1_OFFSET;
+        amountStage1 = amountDue - R_BASE;
+        amountStage2 = 0;
+      } else {
+        amountDue = R_STAGE2 * minutes - R_STAGE2_OFFSET;
+        amountStage1 = R_STAGE1_LIMIT - R_BASE;
+        amountStage2 = amountDue - R_BASE - amountStage1;
+        break;
+      }
   }
+
+  //prepare cout for dollar decimal places
+  cout << fixed << showpoint << setprecision(2);
+  //print results:
+  cout  << "\t\t\n" << "Power Bill For: " << phoneNumber << '\n'
+        << "\t\t" << "============================" << "\n\n"
+        << "Base Payment: " << amountBase << '\n'
+        << "Stage 1: " << amountStage1 << '\n'
+        << "Stage 2: " << amountStage2 << '\n'
+        << "Total Cost: " << amountDue << '\n'
+        ;
 
 }
