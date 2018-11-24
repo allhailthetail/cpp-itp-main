5cc522b9f66e30aee0fafa2aeeb35d79957e4982
Matthew Younger <matthew.younger@icloud.com>
2018-11-23 19:37:07 -0600

    Part B.  Works. Done.

--- LabFiles/lab16.cpp
+++ LabFiles/lab16.cpp
@@ -3,6 +3,10 @@ Matthew Younger
 lab16.cpp
 */
 
+
+/*
+Part A
+
 #include<iostream>
 #include<cmath>
 #include<iomanip>
@@ -62,4 +66,74 @@ double findCircumference(double radius){
   circumference = 2 * pi * radius;
 
   return circumference;
+}
+
+*/
+
+
+// This program takes two numbers (payRate & hours) // and multiplies them to get grosspay.
+// It then calculates net pay by subtracting 15%
+#include <iostream>
+#include <iomanip>
+using namespace std;
+//Function prototypes
+void printDescription();
+void printPay(double&, double&);
+void computePaycheck(double, int, double&, double&);
+
+int main(){
+double payRate;
+double grossPay;
+double netPay;
+int hours;
+
+cout << setprecision(2) << fixed;
+cout << "Welcome to the Pay Roll Program" << '\n';
+
+//prints the description of the program
+printDescription();
+
+cout << "Please input the pay per whole hour" << '\n';
+cin >> payRate;
+cout << "Please input the number of whole hours worked" << '\n';
+cin >> hours;
+
+//computes the values to return for gross & net pay
+computePaycheck(payRate, hours, grossPay, netPay);
+
+//prints the results:
+printPay(grossPay, netPay);
+
+return 0;
+}
+
+void printDescription(){  
+cout  << "\n\n"
+      << "************************************************" << '\n'
+      << "This program takes two numbers (payRate & hours)" << '\n'
+      << "and multiplies them to get gross pay " << '\n'
+      << "it then calculates net pay by subtracting 15%" << '\n'
+      << "************************************************" << "\n\n"
+      ;
+}
+
+// *********************************************************************
+// computePaycheck //
+// task: This function takes rate and time and multiples them to
+// get gross pay and then finds net pay by subtracting 15%.
+// data in: pay rate and time in hours worked
+// data out: the gross and net pay //
+// ********************************************************************
+void computePaycheck(double rate, int time, double &grossPay, double &netPay){
+  grossPay = rate * time;
+  netPay = grossPay * (1.0 - .15);
+}
+
+void printPay(double &grossPay, double &netPay){
+  cout  << "\n\n"
+        << "The Gross Pay Was: $" << grossPay
+        << '\n'
+        << "  The Net Pay Was: $"   << netPay
+        << "\n\n"
+        ;
 }
