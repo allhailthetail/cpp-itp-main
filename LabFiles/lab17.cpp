b6e06dc4d0ada45ff7bbe2a8faf51b47cdbfd99c
Matthew Younger <matthew.younger@icloud.com>
2018-11-30 10:00:50 -0600

    Not sure on 4, 5, 6...

---
+++ LabFiles/lab17.cpp
@@ -0,0 +1,17 @@
+/*
+Matthew Younger
+lab17.cpp
+
+Pre-Lab Questions:
+
+1.  A class constructor is a unique class member function that is
+    automatically called when a class is 'instantiated.'
+2.  Constructors are different because they must have the same name as
+    the class they are assigned to, and constructors have no
+    return type.
+3.  Declaring things inside a class as public is necessary when those items
+    will be used outside the class.  Likewise, classes keep all items
+    private, unless specified otherwise.  In this way, data encapsulation
+    makes object-oriented programming so powerful.
+4, 5, 6?
+*/
