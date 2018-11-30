0f823c38c7a0e62832df752d49bdaa85c4c8e146
Matthew Younger <matthew.younger@icloud.com>
2018-11-30 10:42:06 -0600

    Seems to work exactly as intended...  Proud of myself.  Done.

--- LabFiles/lab17.cpp
+++ LabFiles/lab17.cpp
@@ -15,3 +15,96 @@ Pre-Lab Questions:
     makes object-oriented programming so powerful.
 4, 5, 6?
 */
+
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+class BankAccount
+{
+private:
+  double balance;
+  int accountNumber;
+  double interestRate;
+  string ownerName;
+
+public:
+  BankAccount();  //default constructor
+  BankAccount(double, double, int, string); //overlaoded...
+  double getBalance();
+  void deposit(double);
+  void withdraw(double);
+  void addInterest();
+  void displayAccountSummary();
+  //~BankAccount(); //desructor...  Not required for this lab?
+};
+
+BankAccount::BankAccount(){ //default constructor...
+  balance = 0;
+  accountNumber = 0;
+  interestRate = 0;
+  ownerName = "John Doe";
+}
+
+BankAccount::BankAccount(double bal, double interest, //overloaded consrctor...
+      int acct, string name){
+  balance = bal;
+  accountNumber = acct;
+  interestRate = interest;
+  ownerName = name;
+}
+double BankAccount::getBalance(){
+  return balance;
+}
+
+void BankAccount::deposit(double deposit){
+  //adds deposit ammount to balance...
+  balance += deposit;
+}
+
+void BankAccount::withdraw(double withdraw){
+  //subtracts withdraw from balance...
+  balance -= withdraw;
+}
+
+void BankAccount::addInterest(){
+  double interest;
+  cout << "What's the interest rate in percent??" << '\n';
+  cin >> interest;
+  //convert interest to decimal...
+  interest /= 100;
+  interestRate = interest;
+}
+
+void BankAccount::displayAccountSummary(){
+  cout  << "\n\n"
+        << "Account Number: " << accountNumber << '\n'
+        << "Owner's Name:   " << ownerName     << '\n'
+        ;
+  cout  << fixed << setprecision(2)
+        << "Balance:        " << balance       << '\n'
+        << "Interest Rate:  " << interestRate * 100 << "%" << '\n';
+        ;
+}
+
+//begin main program:
+int main(){
+
+  //call the overload constructor and assign values
+  BankAccount myAccount(1000.50, 0.05, 1111, "John William");
+
+  //call deposit method
+  myAccount.deposit(500);
+
+  //call withdraw method
+  myAccount.withdraw(200);
+
+  //call interest method, if needed...
+  //myAccount.addInterest();
+
+  //call displayAccountSummary method at the end of the program...
+  myAccount.displayAccountSummary();
+
+  return 0;
+}
